105b1c1f0ff6fdc3f8120c5a8f6044240298e9e3
NguyenPhanNhatTu <18110232@student.hcmute.edu.vn>
2021-03-29 08:31:29 +0700

    test Nodejs

---
+++ src/app/Controllers/NewsController.js
@@ -0,0 +1,13 @@
+class NewsController{
+
+    //[Get] /news
+    index(req,res){
+        res.render('news');
+    }
+    show(req,res){
+        res.send('Data ');
+    }
+}
+
+module.exports= new NewsController;
+// const newcontroller= require('/.NewsController')
---
+++ src/routes/site.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const siteController = require('../app/Controllers/SiteController');
+
+//newsController.index()
+router.use('/search',siteController.search)
+
+router.use('/',siteController.index);
+module.exports=router;
