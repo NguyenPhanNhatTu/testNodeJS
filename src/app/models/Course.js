4d57e19919c82be1980129ba5f90c2c163e870b1
NguyenPhanNhatTu <18110232@student.hcmute.edu.vn>
2021-04-07 16:56:27 +0700

    create anđ detail web

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const slug = require('mongoose-slug-generator');
 
+mongoose.plugin(slug);
 
 const Course = new Schema({
-  name: {type:String, maxLength :255},
-  decription: {type:String, maxLength :255},
+  name: {type:String, required: true,},
+  decriptions: {type:String, maxLength :255},
   image: {type:String, maxLength :255},
-  createdAt: {type: Date, default: Date.now },
-  updatedAt: {type: Date, default: Date.now }
+  videoID: {type:String, maxLength :255,required: true,},
+  slug: { type: String, slug: 'name',unique: true },
+
+},{
+  timestamps: true,
 });
 
 module.exports=mongoose.model('Course', Course);
